4159953d8c46ca22e166cba6d47cfe36e90b1ad3
Darkvid Estrella <eclipsecape@gmail.com>
2019-06-18 15:58:46 -0500

    cambios en seleccion

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ var sql = require('mssql');
 var mysql = require('mysql');
 var port = process.env.PORT || 5000;
 
-var config_sql = {
+const config_sql = {
     user: 'darkvid',
-    password: '12345',
+    password: '1234',
     server: '127.0.0.1', // You can use 'localhost\\instance' to connect to named instance
     database: 'Kdbs_jep',
     driver: 'tedious',
@@ -34,7 +34,7 @@ var config_mysql = {
     database: "prestatienda"
 };
 
-const configurarCron = '*/8 * * * *';
+const configurarCron = '*/1 * * * *';
 
 var connection;
 
@@ -64,27 +64,10 @@ handleDisconnect();
 
 console.group('corriendo');
 
-
-
-// capturar detos de sql server
-const sqlServerPromise = new Promise((resolve, reject) => {
-    new sql.ConnectionPool(config_sql).connect().then(pool => {
-        return pool.request().query(`
-        select k.codart as codigo, ROUND(SUM(k.cantot * (44 - ASCII(t.sigdoc))), 2) AS existencia 
-        from kardex k, kardex_tipo_doc t 
-        where k.tipdoc = t.tipdoc and k.codemp=16 GROUP BY k.codemp, k.codalm, k.codart order by 1;`) // esto trae productos trae los id y el stock
-    }).then(result => {
-        sql.close();
-        return resolve(result.recordset);
-    }).catch(err => {
-        sql.close();
-        return reject(err)
-    });
-});
-
 const sqlServerProducts = new Promise((resolve, reject) => {
     new sql.ConnectionPool(config_sql).connect().then(pool => {
-        return pool.request().query(`select codart,prec01,prec02,prec03,prec04 from articulos where codemp=16`)
+        //return pool.request().query(`select codart,prec01,prec02,prec03,prec04 from articulos where codemp=16`)
+        return pool.request().query(`SELECT codart,codalt,desart,nomart,nomcla,nomfam,marca,coduni,poriva,prec01,prec02,prec03,prec04,codpro,nompro,codfab,ultcos,cospro,exiact FROM kdbs_jep.dbo.web_articulos`)
     }).then(result => {
         sql.close();
         return resolve(result.recordset);
@@ -95,53 +78,49 @@ const sqlServerProducts = new Promise((resolve, reject) => {
 });
 
 
+// capturar datos de sql server
+const sqlServeClients = new Promise((resolve, reject) => {
+    new sql.ConnectionPool(config_sql).connect().then(pool => {
+        return pool.request().query(`SELECT  codcli,tipind,rucced,nomcli,repcli,contac,ciucli,dircli,dirnum,dirint,telcli,telcas,telcel,email FROM kdbs_jep.dbo.web_clientes`)
+    }).then(result => {
+        sql.close();
+        return resolve(result.recordset);
+    }).catch(err => {
+        sql.close();
+        return reject(err)
+    });
+});
 
-const promisesSql = [sqlServerPromise, sqlServerProducts];
+
+
+const promisesSql = [sqlServerProducts, sqlServeClients];
 
 const idsProductosPromise = Promise.all(promisesSql).then(results => {
-    const sqlServerData = results[0];
-    const sqlServerDataProducts = results[1];
-    const totalRecords = sqlServerData.length;
+    const sqlServerDataProducts = results[0];
+    const sqlServerDataClients = results[1];
     const totalRecordsProducts = sqlServerDataProducts.length;
-    const idStock = [];
-    const descProd = [];
-    const codart = [];
-    const stockArray = [];
+    const totalRecordsClients = sqlServerDataClients.length;
     const datosStock = [];
-    const preciosArray = [];
+    const datosClientes = [];
 
-    for (let h = 0; h < totalRecords; h++) {
+    for (let h = 0; h < totalRecordsProducts; h++) {
 
         datosStock.push({
-            'idProductoStock': String(sqlServerData[h].codigo).trim(),
-            'stock': sqlServerData[h].existencia,
-            'precios': sqlServerDataProducts.find(precio => String(precio.codart).trim() === String(sqlServerData[h].codigo).trim())
+            'idProductoStock': String(sqlServerDataProducts[h].codalt).trim(),
+            'stock': sqlServerDataProducts[h].exiact,
+            'precios': sqlServerDataProducts.find(precio => String(precio.codalt).trim() === String(sqlServerDataProducts[h].codalt).trim())
 
         });
-        idStock.push("'" + String(sqlServerData[h].codigo).trim() + "'");
 
     }
-    /** Esta es la estructura de los datos para sql server te sirve? simon 
-     * 5470 en total
-     * [ { idProductoStock: '0306051501',
-      stock: 10,
-      precios:
-       { codart: '0306051501          ',
-         prec01: 108.36,
-         prec02: 122.45,
-         prec03: 102.94,
-         prec04: 137.62 } }...
-    ]
-    */
-    return datosStock;
+
+    return { datosStock, sqlServerDataClients };
 });
 
 const mysqlProductPromise = new Promise((resolve, reject) => {
-    // aqui capturo los ids ese idS
-    var stockArray = [];
     return connection.query(`
-        SELECT twofowg1_jepweb.ps_product.id_product,twofowg1_jepweb.ps_product.reference 
-        FROM twofowg1_jepweb.ps_product 
+        SELECT twofowg1_jepnode.ps_product.id_product,twofowg1_jepnode.ps_product.supplier_reference 
+        FROM twofowg1_jepnode.ps_product 
         `, (error_stock, result_stock) => {
         if (!error_stock) {
             return resolve(JSON.parse(JSON.stringify(result_stock)));
@@ -149,16 +128,43 @@ const mysqlProductPromise = new Promise((resolve, reject) => {
         return resolve([]);
     });
 });
+const mysqlClientPromise = new Promise((resolve, reject) => {
+    return connection.query(`
+        SELECT pc.email 
+        from twofowg1_jepnode.ps_customer pc
+        `, (error_clientes, result_clientes) => {
+        if (!error_clientes) {
+            return resolve(JSON.parse(JSON.stringify(result_clientes)));
+        }
+        return resolve([]);
+    });
+});
 
 const dataProductosPromise = Promise.all([mysqlProductPromise, idsProductosPromise]).then(result => {
     const productosMysql = result[0];
-    const productosSqlServer = result[1];
+    const productosSqlServer = result[1]['datosStock'];
     // ahora unir la info de sql server con mysql
     const productos = productosSqlServer.map(producto => {
         return {
             ...producto,
             mysql: productosMysql.find(pm => {
-                return pm.reference.replace(/ /ig, '') == producto.idProductoStock.replace(/ /ig, '');
+                return pm.supplier_reference.replace(/ /ig, '') == producto.idProductoStock.replace(/ /ig, '');
+            })
+        };
+    });
+    return (productos, productos.filter(p => p.mysql));
+});
+const dataClientesPromise = Promise.all([mysqlClientPromise, idsProductosPromise]).then(result => {
+    const clientesMysql = result[0];
+    console.log(clientesMysql);
+    return;
+    const productosSqlServer = result[1]['datosStock'];
+    // ahora unir la info de sql server con mysql
+    const productos = productosSqlServer.map(producto => {
+        return {
+            ...producto,
+            mysql: productosMysql.find(pm => {
+                return pm.supplier_reference.replace(/ /ig, '') == producto.idProductoStock.replace(/ /ig, '');
             })
         };
     });
@@ -166,25 +172,27 @@ const dataProductosPromise = Promise.all([mysqlProductPromise, idsProductosPromi
 });
 
 // aqui a actualizar todos los productos
+/*
 cron.schedule(configurarCron, () => {
     dataProductosPromise.then(productos => {
         //update stock
         productos.forEach(producto => {
-            connection.query(`UPDATE twofowg1_jepweb.ps_stock_available, twofowg1_jepweb.ps_product , twofowg1_jepweb.ps_product_shop
-            SET twofowg1_jepweb.ps_stock_available.quantity = ${producto.stock},twofowg1_jepweb.ps_product.price = ${producto.precios.prec01},twofowg1_jepweb.ps_product_shop.price = ${producto.precios.prec01}
+            connection.query(`UPDATE twofowg1_jepnode.ps_stock_available, twofowg1_jepnode.ps_product , twofowg1_jepnode.ps_product_shop
+            SET twofowg1_jepnode.ps_product.quantity = ${producto.stock},twofowg1_jepnode.ps_stock_available.quantity = ${producto.stock},twofowg1_jepnode.ps_product.price = ${producto.precios.prec01},twofowg1_jepnode.ps_product_shop.price = ${producto.precios.prec01}
             WHERE 
-            twofowg1_jepweb.ps_product.id_product=twofowg1_jepweb.ps_product_shop.id_product
-            AND twofowg1_jepweb.ps_product.id_product=twofowg1_jepweb.ps_stock_available.id_product
-            AND twofowg1_jepweb.ps_stock_available.id_product = ${producto.mysql.id_product};`, (error_stock2, result_stock2) => {
+            twofowg1_jepnode.ps_product.id_product=twofowg1_jepnode.ps_product_shop.id_product
+            AND twofowg1_jepnode.ps_product.id_product=twofowg1_jepnode.ps_stock_available.id_product
+            AND twofowg1_jepnode.ps_stock_available.id_product = ${producto.mysql.id_product};`, (error_stock2, result_stock2) => {
                 if (!error_stock2) {
-                    console.log('actualizado stock ' + producto.mysql.id_product);
+                    console.log(producto.stock,producto.precios.prec01,producto.mysql.id_product);
+                    //console.log('actualizado stock ' + producto.mysql.id_product);
                 } else {
                     console.log(error_stock2);
                 }
             });
             connection.query(`
-            REPLACE INTO twofowg1_jepweb.ps_specific_price
-            (twofowg1_jepweb.ps_specific_price.id_product,twofowg1_jepweb.ps_specific_price.id_shop,twofowg1_jepweb.ps_specific_price.id_currency,twofowg1_jepweb.ps_specific_price.id_country,twofowg1_jepweb.ps_specific_price.id_group,twofowg1_jepweb.ps_specific_price.price,twofowg1_jepweb.ps_specific_price.reduction_type) 
+            REPLACE INTO twofowg1_jepnode.ps_specific_price
+            (twofowg1_jepnode.ps_specific_price.id_product,twofowg1_jepnode.ps_specific_price.id_shop,twofowg1_jepnode.ps_specific_price.id_currency,twofowg1_jepnode.ps_specific_price.id_country,twofowg1_jepnode.ps_specific_price.id_group,twofowg1_jepnode.ps_specific_price.price,twofowg1_jepnode.ps_specific_price.reduction_type) 
             VALUES
             (${producto.mysql.id_product},1,1,81,3,${producto.precios.prec01},"amount"),
             (${producto.mysql.id_product},1,1,81,4,${producto.precios.prec02},"amount"),
@@ -202,3 +210,6 @@ cron.schedule(configurarCron, () => {
         //update precios
     });
 });
+
+// aqui a actualizar todos los clientes
+*/
